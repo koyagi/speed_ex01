c67116feb82fcc9af14ba7102f8e1c79b66c1305
kosuke <no-define>
2023-01-03 11:07:40 +0900

    refactor: create decks step1

--- dealer.cpp
+++ dealer.cpp
@@ -9,18 +9,20 @@ int main() {
     using namespace std::string_literals;
     std::cout << "howdy!"s << std::endl;
 
-    const auto suits_red = std::vector<std::string>{ "D"s, "H"s };
-    const auto suits_black = std::vector<std::string>{ "S"s, "C"s };
-    const auto number = std::vector<std::string>{"A"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s, "10"s, "J"s, "Q"s, "K"s};
     auto deck_red = std::vector<std::string>{};
-    deck_red.reserve(26);
     auto deck_black = std::vector<std::string>{};
-    deck_black.reserve(26);
-    for(int i = 0; i < 26; i++) {
-        deck_red.push_back(suits_red.at(i / 13) + number.at(i % 13));
-    }
-    for(int i = 0; i < 26; i++) {
-        deck_black.push_back(suits_black.at(i / 13) + number.at(i % 13));
+    {
+        deck_red.reserve(26);
+        deck_black.reserve(26);
+        const auto suits_red = std::vector<std::string>{ "D"s, "H"s };
+        const auto suits_black = std::vector<std::string>{ "S"s, "C"s };
+        const auto number = std::vector<std::string>{"A"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s, "10"s, "J"s, "Q"s, "K"s};
+        for(int i = 0; i < 26; i++) {
+            deck_red.push_back(suits_red.at(i / 13) + number.at(i % 13));
+        }
+        for(int i = 0; i < 26; i++) {
+            deck_black.push_back(suits_black.at(i / 13) + number.at(i % 13));
+        }
     }
     for(const auto el : deck_red ) {
         std::cout << el << ", ";
