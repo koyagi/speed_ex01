9e540409f3a140fb6a855768ebe961ba73219e0b
kosuke <no-define>
2023-01-04 07:33:59 +0900

    refactor: step4

--- dealer.cpp
+++ dealer.cpp
@@ -1,30 +1,9 @@
+#include "dealer.hpp"
+
 #include <iostream>
 #include <string>
-#include <tuple>
 #include <vector>
 #include <cstdlib>
-
-namespace {
-    using namespace std::string_literals;
-    auto create_akakuro_decks() {
-        auto deck_red = std::vector<std::string>{};
-        auto deck_black = std::vector<std::string>{};
-        deck_red.reserve(26);
-        deck_black.reserve(26);
-        const auto suits_red = std::vector<std::string>{ "D"s, "H"s };
-        const auto suits_black = std::vector<std::string>{ "S"s, "C"s };
-        const auto rank = std::vector<std::string>{"A"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s, "10"s, "J"s, "Q"s, "K"s};
-        for(int i = 0; i < 26; i++) {
-            deck_red.push_back(suits_red.at(i / 13) + rank.at(i % 13));
-        }
-        for(int i = 0; i < 26; i++) {
-            deck_black.push_back(suits_black.at(i / 13) + rank.at(i % 13));
-        }
-        return std::make_tuple(deck_red, deck_black);
-    }
-}
-
-
 
 int main() {
     using namespace std::string_literals;
@@ -33,11 +12,11 @@ int main() {
     auto [pile_red, pile_black] = create_akakuro_decks();
 
     for(const auto el : pile_red ) {
-        std::cout << el << ", ";
+        std::cout << el << ", "s;
     }
     std::cout << std::endl;
     for(const auto el : pile_black ) {
-        std::cout << el << ", ";
+        std::cout << el << ", "s;
     }
     std::cout << std::endl;
     std::cout << "bye!"s << std::endl;
