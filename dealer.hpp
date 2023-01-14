e0812bb5c233619326e82703b8c4e509b6c9e78f
kosyago <nodefine@example.com>
2023-01-14 18:12:11 +0900

    modify: simplify and connstness

--- dealer.hpp
+++ dealer.hpp
@@ -9,12 +9,12 @@ namespace {
     using strVec = std::vector<std::string>;
 
     template<typename T = const strVec>
-    auto create_half_deck( T && suits) {
+    auto create_half_deck( T && suits) {    // Here, argument suits should be const universal-refarence.
         const auto rank = strVec{"A"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s, "10"s, "J"s, "Q"s, "K"s};
         auto deck = strVec{};
         deck.reserve( suits.size() * rank.size() );
         for(const auto & aSuit : suits) {
-            std::transform(std::begin(rank), std::end(rank), std::back_inserter(deck), [suit = aSuit](std::string aRank){return suit + aRank;});
+            std::transform(std::cbegin(rank), std::cend(rank), std::back_inserter(deck), [suit = aSuit](std::string aRank){return suit + aRank;});
         }
         return deck;
     }
@@ -25,10 +25,9 @@ namespace {
         std::shuffle(std::begin(deck), std::end(deck), randomEngine);
     }
 
-    auto create_akakuro_decks() {   // FIXME: need refactoring
+    auto create_akakuro_decks() {
         auto deck_red = create_half_deck({ "D"s, "H"s });
-        const auto suits_black = strVec{ "S"s, "C"s };
-        auto deck_black = create_half_deck(suits_black);
+        auto deck_black = create_half_deck({ "S"s, "C"s });
         cardShuffle(deck_red);
         cardShuffle(deck_black);
         return std::make_tuple(deck_red, deck_black);
